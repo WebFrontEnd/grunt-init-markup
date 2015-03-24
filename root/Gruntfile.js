6c1fc17048d1e60c583626394203ea7848240aa2
ju.uyeong <ju.uyeong@nhn.com>
2015-03-24 23:44:37 +0900

    feat(grunt): imagemin(newer 사용) 태스크 생성
    #3

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -52,6 +52,19 @@ module.exports = function(grunt) {
                 }
             }
         },
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 3
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'img',
+                    src: ['**/*.{png,jpg,gif}', '!sp/**/*.{png,jpg,gif}'],
+                    dest: 'img'
+                }]
+            }
+        },
         concat: {
             sprites: {
                 files: {
@@ -183,7 +196,7 @@ module.exports = function(grunt) {
     });
 
     // CSS task(s).
-    grunt.registerTask('css', ['sprite', 'image_resize', 'concat', 'scsslint', 'sass:dev', 'csslint', 'autoprefixer:dev']);
+    grunt.registerTask('css', ['sprite', 'image_resize', 'newer:imagemin', 'concat', 'scsslint', 'sass:dev', 'csslint', 'autoprefixer:dev']);
 
     // HTML task(s).
     grunt.registerTask('html', ['includereplace', 'htmlhint', 'validation']);
@@ -192,7 +205,7 @@ module.exports = function(grunt) {
     grunt.registerTask('devel', ['css', 'html']);
 
     // Build task(s).
-    grunt.registerTask('build', ['sprite', 'image_resize', 'concat', 'scsslint', 'sass:min', 'csslint', 'autoprefixer:min', 'html']);
+    grunt.registerTask('build', ['sprite', 'image_resize', 'newer:imagemin', 'concat', 'scsslint', 'sass:min', 'csslint', 'autoprefixer:min', 'html']);
 
     // Default task(s).
     grunt.registerTask('default', ['devel', 'build']);
